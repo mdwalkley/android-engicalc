c897110fa10affa08f5e8091d4fe61b3aa6e3866
Michael Walkley <michael_walkley@senecacenter.org>
2018-04-20 11:08:28 -0700

    Initial Commit.

---
+++ app/src/main/java/us/walkley/mw/engicalc/MainActivity.java
@@ -0,0 +1,13 @@
+package us.walkley.mw.engicalc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
