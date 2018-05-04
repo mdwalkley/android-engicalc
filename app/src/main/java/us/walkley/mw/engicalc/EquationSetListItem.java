2ee8b8c1d2cfc0f4cfc4b8a8a46f287dcd27e424
Michael Walkley <michael_walkley@senecacenter.org>
2018-05-04 15:57:49 -0700

    Change layout file names for better organization.

---
+++ app/src/main/java/us/walkley/mw/engicalc/EquationSetListItem.java
@@ -0,0 +1,8 @@
+package us.walkley.mw.engicalc;
+
+/**
+ * Created by michael_walkley on 5/4/2018.
+ */
+
+public class EquationSetListItem {
+}
---
+++ app/src/main/java/us/walkley/mw/engicalc/ListAdapter_MainActivity.java
@@ -0,0 +1,10 @@
+package us.walkley.mw.engicalc;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by michael_walkley on 5/4/2018.
+ */
+
+public class ListAdapter_MainActivity extends ArrayAdapter {
+}
