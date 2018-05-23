0f7927db36a06eccff4fe45987a35f29ffc4d096
Michael Walkley <michael_walkley@senecacenter.org>
2018-05-23 16:59:24 -0700

    Begin adding AnswerFragment files to project.

---
+++ app/src/main/java/us/walkley/mw/engicalc/AnswerItemFragment.java
@@ -0,0 +1,79 @@
+package us.walkley.mw.engicalc;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import us.walkley.mw.engicalc.dummy.DummyContent;
+import us.walkley.mw.engicalc.dummy.DummyContent.DummyItem;
+
+/**
+ * A fragment representing a list of Items.
+ */
+public class AnswerItemFragment extends Fragment {
+
+    // TODO: Customize parameter argument names
+    private static final String ARG_COLUMN_COUNT = "column-count";
+    // TODO: Customize parameters
+    private int mColumnCount = 1;
+    private ArrayList<AnswerItem> answerList;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public AnswerItemFragment() {
+    }
+
+    // TODO: Customize parameter initialization
+    @SuppressWarnings("unused")
+    public static AnswerItemFragment newInstance(int columnCount) {
+        AnswerItemFragment fragment = new AnswerItemFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_COLUMN_COUNT, columnCount);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments() != null) {
+            mColumnCount = getArguments().getInt(ARG_COLUMN_COUNT);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_answeritem_list, container, false);
+
+        // Set the adapter
+        if (view instanceof RecyclerView) {
+            Context context = view.getContext();
+            RecyclerView recyclerView = (RecyclerView) view;
+            if (mColumnCount <= 1) {
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+            } else {
+                recyclerView.setLayoutManager(new GridLayoutManager(context, mColumnCount));
+            }
+            recyclerView.setAdapter(new AnswerItemRecyclerViewAdapter(answerList));
+        }
+        return view;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
---
+++ app/src/main/java/us/walkley/mw/engicalc/AnswerItemRecyclerViewAdapter.java
@@ -0,0 +1,59 @@
+package us.walkley.mw.engicalc;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link AnswerItem}
+ */
+public class AnswerItemRecyclerViewAdapter extends RecyclerView.Adapter<AnswerItemRecyclerViewAdapter.ViewHolder> {
+
+    private final List<AnswerItem> mValues;
+
+    public AnswerItemRecyclerViewAdapter(List<AnswerItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.fragment_answeritem, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        AnswerItem currentItem = mValues.get(position);
+        holder.mImageView.setImageResource(currentItem.getImgSrc());
+        holder.mTextView.setText(Double.toString(currentItem.getAnswer()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+        public final ImageView mImageView;
+        public final TextView mTextView;
+
+        public ViewHolder(View view) {
+            super(view);
+            mView = view;
+            mImageView = (ImageView) view.findViewById(R.id.item_number);
+            mTextView = (TextView) view.findViewById(R.id.content);
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mTextView.getText() + "'";
+        }
+    }
+}
